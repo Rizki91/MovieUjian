20617669e6379894fa951aa3c4133233f5bc55ea
Rizki91 <fahrulrizki991@gmail.com>
2020-09-05 17:59:48 +0700

    Initial commit

---
+++ app/src/main/java/com/fahrul/movieujian/TambahMovie.java
@@ -0,0 +1,14 @@
+package com.fahrul.movieujian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TambahMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_movie);
+    }
+}
